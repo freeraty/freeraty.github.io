6e2b85f500989cfd1e537dc3b1642537fea2a6fe
paulosergioduff <paulosergioduff@gmail.com>
2018-06-24 18:24:10 -0300

    Estrutura de toda front-end

---
+++ js/rotas.js
@@ -0,0 +1,16 @@
+
+var amigavel = false;
+var rota = ".html";
+
+	if (amigavel == true) {
+		var rota = "";
+		console.log('urls amigáveis habilitadas no sistema: index'+rota);
+	}
+		else{
+			console.log('index'+rota);
+		}
+
+function vaiPara($link){
+	pagina = $link+rota;
+	console.log(pagina);
+}
---
+++ novoEstilo/js/packMenu.js
@@ -0,0 +1,4 @@
+var packMenu = '<li class="nav-item"><a class="nav-link" onclick="vaiPara('+"'"+'comoFunciona'+"'"+');">Como funciona</a></li><li class="nav-item"><a class="nav-link" onclick="vaiPara('+"'"+'politicasDeUso'+"'"+');">Política de uso</a></li>';
+var expandeMenu = "";
+
+document.getElementById('packMenu').innerHTML = packMenu+expandeMenu;
